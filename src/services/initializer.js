400e8f6b17cd5e6b5fe9a6382d8edf8a99c4dd96
nvan23 <dtnvan230898@gmail.com>
2021-06-21 22:17:51 +0700

    adding initialer for host and home account

--- src/services/initializer.js
+++ src/services/initializer.js
@@ -2,8 +2,10 @@
 
 const config = require('../config')
 const Role = require('../models/role.model')
+const User = require('../models/user.model')
+const Home = require('../models/home.model')
 
-exports.roles = async (req, res) => {
+exports.roles = async () => {
   const roles = await Role.find()
 
   if (roles.length) return
@@ -12,5 +14,54 @@ exports.roles = async (req, res) => {
   for (const [key, value] of Object.entries(rawRoles)) {
     const role = new Role({ name: key, key: value })
     await role.save()
+  }
+}
+
+exports.host = async () => {
+  try {
+    const users = await User.find()
+    if (users?.length) throw { error: "Error during init default host account." }
+
+    const user = new User(config?.default?.host)
+    await user.save()
+    await user.initHostRole()
+
+    console.log({ msg: 'Init default host account successfully' })
+
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+exports.home = async () => {
+  try {
+    const users = await User.find()
+    if (users?.length !== 1) throw { error: "Error during create default home." }
+
+    const homes = await Home.find()
+    if (homes?.length !== 1) throw { error: "Error during create default home." }
+
+    const home = new Home({
+      name: `${users[0]?.name}'s home`,
+      hostId: users[0]?.id,
+    })
+
+    if (!home) throw { error: "Cannot create a first home" }
+    await home.save()
+
+    await User
+      .findByIdAndUpdate(
+        users[0]?.id,
+        {
+          homeId: home._id,
+          currentHome: home._id,
+        },
+        { new: true }
+      )
+
+    console.log("Creating default home successfully")
+
+  } catch (error) {
+    console.log(error)
   }
 }
