a8685f72fb47c80413b7c5846dc7eca46198e0b1
nvan23 <dtnvan230898@gmail.com>
2021-09-22 18:59:29 +0700

    remove code comment

--- src/controllers/room/device/index.js
+++ src/controllers/room/device/index.js
@@ -110,41 +110,6 @@ exports.turnOn = (status) => {
   }
 }
 
-// exports.turnOnAuto = (status) => {
-//   return async function (req, res) {
-//     try {
-//       const user = await User.findById(req.user.id)
-//       if (!user) throw { error: "User not found" }
-
-//       if (!user?.homeId && !user?.rooms.some(r => r.roomId.toString() === req?.room?.id))
-//         throw { error: "Member does not host or Member is not in this room" }
-
-//       if (!checker.isObjectId(req?.params?.id))
-//         throw { error: "Invalid input" }
-
-//       const device = await Device.findById(req?.params?.id)
-//       if (!device) throw { error: "Device not found." }
-
-//       if (device.roomId !== req?.room?.id)
-//         throw { error: "This device not in room" }
-
-//       if (!device?.isLive) throw { error: "Device is off" }
-
-//       mqttClient.publish(`${device?.topic}/auto` || 'unknown', status ? '1' : '0')
-
-//       device.autoRunStatus = status
-//       await device.save()
-
-//       res.status(200).json({
-//         deviceId: device?.id,
-//         status: device?.autoRunStatus,
-//       })
-//     } catch (error) {
-//       res.status(400).json(error)
-//     }
-//   }
-// }
-
 exports.removeDevice = async (req, res) => {
   try {
     if (
