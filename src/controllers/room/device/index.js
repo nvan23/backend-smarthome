e051bc961ad33898d8f9c710cbab918f910d0382
nvan23 <dtnvan230898@gmail.com>
2021-09-22 18:57:37 +0700

    implement enable/disable auto-run

--- src/controllers/room/device/index.js
+++ src/controllers/room/device/index.js
@@ -2,6 +2,7 @@
 
 const Room = require("../../../models/room.model")
 const Device = require("../../../models/device.model")
+const User = require("../../../models/user.model")
 
 const checker = require('../../../utils/checker')
 
@@ -79,11 +80,20 @@ exports.addDevice = async (req, res) => {
 exports.turnOn = (status) => {
   return async function (req, res) {
     try {
+      const user = await User.findById(req.user.id)
+      if (!user) throw { error: "User not found" }
+
+      if (!user?.homeId && !user?.rooms.some(r => r.roomId.toString() === req?.room?.id))
+        throw { error: "Member does not host or Member is not in this room" }
+
       if (!checker.isObjectId(req?.params?.id))
         throw { error: "Invalid input" }
 
       const device = await Device.findById(req?.params?.id)
       if (!device) throw { error: "Device not found." }
+
+      if (device.roomId !== req?.room?.id)
+        throw { error: "This device not in room" }
 
       mqttClient.publish(device?.topic || 'unknown', status ? '1' : '0')
 
@@ -99,6 +109,41 @@ exports.turnOn = (status) => {
     }
   }
 }
+
+// exports.turnOnAuto = (status) => {
+//   return async function (req, res) {
+//     try {
+//       const user = await User.findById(req.user.id)
+//       if (!user) throw { error: "User not found" }
+
+//       if (!user?.homeId && !user?.rooms.some(r => r.roomId.toString() === req?.room?.id))
+//         throw { error: "Member does not host or Member is not in this room" }
+
+//       if (!checker.isObjectId(req?.params?.id))
+//         throw { error: "Invalid input" }
+
+//       const device = await Device.findById(req?.params?.id)
+//       if (!device) throw { error: "Device not found." }
+
+//       if (device.roomId !== req?.room?.id)
+//         throw { error: "This device not in room" }
+
+//       if (!device?.isLive) throw { error: "Device is off" }
+
+//       mqttClient.publish(`${device?.topic}/auto` || 'unknown', status ? '1' : '0')
+
+//       device.autoRunStatus = status
+//       await device.save()
+
+//       res.status(200).json({
+//         deviceId: device?.id,
+//         status: device?.autoRunStatus,
+//       })
+//     } catch (error) {
+//       res.status(400).json(error)
+//     }
+//   }
+// }
 
 exports.removeDevice = async (req, res) => {
   try {
--- src/middleware/requireInRoom.js
+++ src/middleware/requireInRoom.js
@@ -1,13 +1,17 @@
 const Room = require('../models/room.model')
+const User = require('../models/user.model')
 
 const requireInRoom = () => {
   return async function (req, res, next) {
     try {
+      const user = await User.findById(req.user.id)
+      if (!user) throw { error: "User not found" }
+
       const room = await Room.findById(req.room.id)
       if (!room) throw { error: "Room not found" }
 
-      const userInRoom = room?.members.some(m => m?.userId.toString() === req.user.id)
-      if (!userInRoom)
+      const userInRoom = user?.rooms.some(r => r?.roomId.toString() === req?.room?.id.toString())
+      if (!user?.homeId && !userInRoom)
         throw { error: "You not in this room" }
 
       next()
